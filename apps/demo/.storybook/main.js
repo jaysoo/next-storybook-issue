a69c0b125d723d3b9bf296fa04014acfe7c0cb29
Jack Hsu <jack.hsu@gmail.com>
2023-04-17 16:01:26 -0400

    fix config

--- apps/demo/.storybook/main.js
+++ apps/demo/.storybook/main.js
@@ -5,20 +5,22 @@ const path = require('path');
 module.exports = {
   core: { builder: 'webpack5' },
   stories: [
-    
+
     '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials' , '@nrwl/react/plugins/storybook' 
-    
-    , 'storybook-addon-swc', 
-        {
+  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'
+
+    ,
+
+    //'storybook-addon-swc', 
+    {
       name: 'storybook-addon-next',
       options: {
         nextConfigPath: path.resolve(__dirname, '../next.config.js'),
       },
     }
-     
+
   ]
 };
 
